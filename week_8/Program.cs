7826905be3aee93eaab5023cf1e747a573bcfd02
Alyssa Colistro <arcolistro@gmail.com>
2020-03-02 15:59:14 -0800

    Add inital menu in program.

--- week_8/Program.cs
+++ week_8/Program.cs
@@ -4,11 +4,28 @@ using Bakery.Models;
 namespace Bakery.Models
 {
 
-    class Program
+  class Program
+  {
+    static void Main()
     {
-        static void Main()
-        {
-
-        }
+      Console.WriteLine("Welcome to Fresh Out the Coven Bakery!");
+      Console.WriteLine("Please take a look at our menu before placing your order:");
+      Console.WriteLine("===========================================");
+      Console.WriteLine("   Breads   $5      ||      Pastries  $2   ");
+      Console.WriteLine("-------------------------------------------");
+      Console.WriteLine("      French        ||      Sweet Bun      ");
+      Console.WriteLine("     Italian        ||     Cinnamon Bun    ");
+      Console.WriteLine("       Rye          ||       Danish        ");
+      Console.WriteLine("     Sourdough      ||      Croissant      ");
+      Console.WriteLine("      Brioche       ||       Eclaire       ");
+      Console.WriteLine("    Whole Grain     ||      Bear Claw      ");
+      Console.WriteLine("");
+      Console.WriteLine("");
+      Console.WriteLine("");
+      Console.WriteLine("");
+      Console.WriteLine("");
+      Console.WriteLine("");
+      Console.WriteLine("");
     }
+  }
 }
